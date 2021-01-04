038a12b9a0b6b4ed5ada0486ff0f64af5c229b88
Divine Hycenth <divinehycenth@outlook.com>
2021-01-04 09:46:45 +0100

    Added Adress, Payment, OrderItem and Order models

--- server/core/models.py
+++ server/core/models.py
@@ -19,3 +19,76 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_data = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(
+        'Address', related_name='shipping_address',
+        on_delete=models.SET_NULL, blank=True, null=True
+    )
+    billing_address = models.ForeignKey(
+        'Address', related_name='billing_address',
+        on_delete=models.SET_NULL, blank=True, null=True
+    )
+    payment = models.ForeignKey(
+        'Payment', on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+
+        for order_item in self.items.all():
+            total += order_item.get_total_item_price()
+        return total
+
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
